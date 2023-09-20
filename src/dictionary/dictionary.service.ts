036dfe1e3d1b629d90ce2fa0fcf864ee8e50c6bf
LuiFeAn <luisfernandogvv@gmail.com>
2023-09-20 19:26:11 -0300

    refactor: Removendo métodos não utilizados

--- src/dictionary/dictionary.service.ts
+++ src/dictionary/dictionary.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDictionaryDto } from './dto/create-dictionary.dto';
-import { UpdateDictionaryDto } from './dto/update-dictionary.dto';
 
 @Injectable()
 export class DictionaryService {
-  create(createDictionaryDto: CreateDictionaryDto) {
-    return 'This action adds a new dictionary';
-  }
 
   findAll() {
     return `This action returns all dictionary`;
@@ -16,11 +11,4 @@ export class DictionaryService {
     return `This action returns a #${id} dictionary`;
   }
 
-  update(id: number, updateDictionaryDto: UpdateDictionaryDto) {
-    return `This action updates a #${id} dictionary`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} dictionary`;
-  }
 }
