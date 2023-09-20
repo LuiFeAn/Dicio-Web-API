29ef4b11043f1e2f4be2b1cdf913c439a0b3827a
LuiFeAn <luisfernandogvv@gmail.com>
2023-09-20 19:25:44 -0300

    refactor: Removendo métodos que não serão utilizados do Controller e do Service de dictionary

--- src/dictionary/dictionary.controller.ts
+++ src/dictionary/dictionary.controller.ts
@@ -1,16 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DictionaryService } from './dictionary.service';
-import { CreateDictionaryDto } from './dto/create-dictionary.dto';
-import { UpdateDictionaryDto } from './dto/update-dictionary.dto';
 
 @Controller('dictionary')
 export class DictionaryController {
   constructor(private readonly dictionaryService: DictionaryService) {}
-
-  @Post()
-  create(@Body() createDictionaryDto: CreateDictionaryDto) {
-    return this.dictionaryService.create(createDictionaryDto);
-  }
 
   @Get()
   findAll() {
@@ -22,13 +15,4 @@ export class DictionaryController {
     return this.dictionaryService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDictionaryDto: UpdateDictionaryDto) {
-    return this.dictionaryService.update(+id, updateDictionaryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.dictionaryService.remove(+id);
-  }
 }
